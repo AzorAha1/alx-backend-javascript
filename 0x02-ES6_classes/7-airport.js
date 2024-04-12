43c14e1504980718fa55e1edd5dab399af174be8
AzorAha1 <wizfaiz@icloud.com>
2024-04-12 19:13:58 +0100

    7-airport.js

---
+++ 0x02-ES6_classes/7-airport.js
@@ -0,0 +1,13 @@
+export default class Airport {
+  constructor(name, code) {
+    this._name = name;
+    this._code = code;
+
+    if (typeof name !== 'string') throw TypeError('name must be string');
+    if (typeof code !== 'string') throw TypeError('code must be string');
+  }
+
+  toString() {
+    return this._code;
+  }
+}
