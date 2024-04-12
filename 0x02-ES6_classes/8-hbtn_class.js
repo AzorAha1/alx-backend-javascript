8e316402b6850b44b3f83e0d22f6981365f314c1
AzorAha1 <wizfaiz@icloud.com>
2024-04-12 19:34:36 +0100

    8-hbtn_class.js

--- 0x02-ES6_classes/8-hbtn_class.js
+++ 0x02-ES6_classes/8-hbtn_class.js
@@ -1,16 +1,17 @@
 export default class HolbertonClass {
-    constructor(size, location){
-        this._size = size
-        this._location = location
+  constructor(size, location) {
+    this._size = size;
+    this._location = location;
 
-        if (typeof size !== 'number') throw TypeError('size must be number')
-        if (typeof location !== 'string') throw TypeError('location must be string')
-    }
+    if (typeof size !== 'number') throw TypeError('size must be number');
+    if (typeof location !== 'string') throw TypeError('location must be string');
+  }
 
-    toString(){
-        return this._location
-    }
-    valueOf(){
-        return this._size
-    }
+  toString() {
+    return this._location;
+  }
+
+  valueOf() {
+    return this._size;
+  }
 }
