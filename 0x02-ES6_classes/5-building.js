a44fb15d7cf029598669762b0b8746c8b4bf2052
AzorAha1 <wizfaiz@icloud.com>
2024-04-12 18:24:37 +0100

    5-building.js

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,31 +1,14 @@
 export default class Building {
-    static isAbstract = true
-    constructor(sqrt){
-        
-        this._sqrt = sqrt
-        if (this.constructor.isAbstract){
-            throw new Error('this is an abstract class')
-        }
-        if (typeof this.evacuationWarningMessage !== 'function'){
-            throw new Error('Class extending Building must override evacuationWarningMessage')
-        }
+  constructor(sqft) {
+    this._sqft = sqft;
+    if (this.constructor !== Building) {
+      if (!this.evacuationWarningMessage) {
+        throw new Error('Class extending Building must override evacuationWarningMessage');
+      }
     }
-    set sqrt(thesqrt){
-        if (typeof thesqrt !== 'number'){
-            throw new TypeError('sqrt must be a number')
-        }
-        else{
-            this._sqrt = thesqrt
-        }
-    }
-    
-    get sqrt(){
-        return this._sqrt
-    }
-    evacuationWarningMessage(){
-        throw new Error('evacuationWarningMessage must be implemented by classes that is extended from')
-    }
+  }
 
-   
-    
+  get sqft() {
+    return this._sqft;
+  }
 }
