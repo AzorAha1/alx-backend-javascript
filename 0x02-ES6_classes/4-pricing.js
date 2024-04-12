5bb1b4018942ce14aa4eaafe11dfb6962fc982f7
AzorAha1 <wizfaiz@icloud.com>
2024-04-12 03:01:40 +0100

    4-pricing.js

--- 0x02-ES6_classes/4-pricing.js
+++ 0x02-ES6_classes/4-pricing.js
@@ -1,32 +1,36 @@
-import Currency from "./3-currency"
+export default class Pricing {
+  constructor(amount, currency) {
+    this.amount = amount;
+    this.currency = currency;
+  }
 
-export default class Pricing{
-    constructor(amount, currency){
-        this.amount = amount
-        this.currency = currency
-    }
-    /**
+  /**
      * @param {any} theamount
      */
-    set amount(theamount){
-        this._amount = theamount
-    }
-    /**
+  set amount(theamount) {
+    this._amount = theamount;
+  }
+
+  /**
      * @param {any} thecurrency
      */
-    set currency(thecurrency){
-        this._currency = thecurrency
-    }
-    get amount(){
-        return this._currency
-    }
-    get currency(){
-        return this._currency
-    }
-    displayFullPrice(){
-        return `${this._amount} ${this._currency.name} (${this._currency.code})`
-    }
-    static convertPrice(amount, ConversionRate){
-        return amount * ConversionRate
-    }
+  set currency(thecurrency) {
+    this._currency = thecurrency;
+  }
+
+  get amount() {
+    return this._currency;
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  displayFullPrice() {
+    return `${this._amount} ${this._currency.name} (${this._currency.code})`;
+  }
+
+  static convertPrice(amount, ConversionRate) {
+    return amount * ConversionRate;
+  }
 }
