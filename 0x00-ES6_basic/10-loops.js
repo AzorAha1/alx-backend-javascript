f57e87c4dc4b5c5881d298965db00f750b397d60
AzorAha1 <wizfaiz@icloud.com>
2024-04-13 04:06:13 +0100

    10-loops.js

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,8 +1,10 @@
 export default function appendToEachArrayValue(array, appendString) {
-  const addarray = [];
-  for (const value of array) {
-    addarray.push(appendString + value);
+    const addarray = []
+    for (const value of array) {
+    //   const value = array[idx];
+    //   array[idx] = appendString + value;
+        addarray.push(appendString + value)
+    }
+  
+    return addarray;
   }
-
-  return addarray;
-}
