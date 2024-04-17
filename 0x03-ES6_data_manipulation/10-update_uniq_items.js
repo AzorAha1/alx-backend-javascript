312988a15d2c5699baaff56b6c7586799a82e3df
AzorAha1 <wizfaiz@icloud.com>
2024-04-17 05:22:59 +0100

    10-update_uniq_items.js

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -1,4 +1,7 @@
 export default function updateUniqueItems(map) {
+    if (!(map instanceof Map)){
+        throw new Error('Cannot process')
+    }
   const filteredmap = Array.from(map.entries()).filter(([, value]) => value === 1);
   filteredmap.forEach(([key]) => {
     map.set(key, 100);
