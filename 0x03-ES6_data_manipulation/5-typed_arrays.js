3899c86d7f066bb529fe8c1454706f4c4e56907c
AzorAha1 <wizfaiz@icloud.com>
2024-04-17 03:53:22 +0100

    5-typed_arrays.js

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,8 +1,11 @@
 export default function createInt8TypedArray(length, position, value) {
-  if (position < 0 || position >= length) throw Error('Positon outside range');
+  if (position < 0 || position >= length){
+    throw new Error('Positon outside range')
+  };
   const buffer = new ArrayBuffer(length);
   const theview = new Int8Array(buffer);
 
-  theview[position] = value;
+//   theview.set([value], position);
+  theview[position] = value
   return new DataView(buffer);
 }
