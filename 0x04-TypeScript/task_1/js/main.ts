078f989014881c2c1627338d3482bebdbe7a44ce
AzorAha1 <wizfaiz@icloud.com>
2024-05-03 11:58:06 +0100

    yes

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -6,3 +6,28 @@ interface Teacher {
     location: string
     [key : string]: any
 }
+
+interface Directors extends Teacher{
+    numberOfReports: number
+}
+
+function printTeacher(firstname:string, lastName: string): string {
+    return `${firstname} ${lastName}`
+}
+
+interface printTeacherFucnction {
+    (firstName: string, lastName: string): string
+}
+
+class StudentClass {
+    constructor(firstName: string, lastName: string) {
+        firstName = firstName
+        lastName = lastName
+    }
+    workOnHomework(){
+        return 'Currently working'
+    }
+    displayName(){
+        return student
+    }
+}
