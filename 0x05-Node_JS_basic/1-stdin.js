94310a9125de409c6f91fe06b9403435b3082ef2
root <AzorAha1>
2024-06-04 22:15:11 +0000

    1-stdin.js

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -2,6 +2,6 @@ process.stdout.write('Welcome to Holberton School, what is your name?\n');
 process.stdin.on('data', (data) => {
   const name = data.toString().trim();
   process.stdout.write(`Your name is ${name}\n`);
-  console.log('This important software is now closing\n')
+  console.log('This important software is now closing')
   process.exit(0);
 });
