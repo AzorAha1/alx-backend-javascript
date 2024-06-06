58f6eb73f2c9dddac0c9c47ce92509f53ee93345
AzorAha1 <wizfaiz@icloud.com>
2024-06-06 20:57:42 +0000

    yes

--- 0x06-unittests_in_js/0-calcul.js
+++ 0x06-unittests_in_js/0-calcul.js
@@ -1,6 +1,5 @@
 function calculateNumber(a, b){
-	const rounda = Math.round(a);
-	const roundb = Math.round(b);
-	return rounda + roundb;
+	return Math.round(a) + Math.round(b);
 }
+
 module.exports = calculateNumber;
