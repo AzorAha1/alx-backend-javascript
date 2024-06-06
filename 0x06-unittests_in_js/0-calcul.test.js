f15660fbed009aa029c78f568b069ce15ec8ed0e
AzorAha1 <wizfaiz@icloud.com>
2024-06-06 14:50:06 +0000

    done

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -1,6 +1,7 @@
 const assert = require('assert')
 const calculateNumber = require('./0-calcul');
-describe('sum test', function() {
+describe('calculateNumber', function() {
+	describe('sum test', function() {
 	it('check sum of a and b', function() {
 		assert.equal(calculateNumber(1, 3), 4)
 		assert.equal(calculateNumber(1, 3.7), 5)
@@ -8,3 +9,5 @@ describe('sum test', function() {
 		assert.equal(calculateNumber(1.5, 3.7), 6)	
 	})
 })
+})
+
