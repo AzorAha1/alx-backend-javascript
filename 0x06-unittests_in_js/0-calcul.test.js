d4a5a576507f1c48618201d5662b6fb3da846e0b
root <AzorAha1>
2024-06-06 14:03:40 +0000

    0-calcul.test.js

---
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -0,0 +1,9 @@
+const assert = require('assert')
+describe('sum test', function() {
+	it('check sum of a and b', function() {
+		a = 5
+		b = 6
+		const result = 5 + 6
+		assert.equal(result, 11)
+	})
+})
