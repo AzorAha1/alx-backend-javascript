555d9e42e945a9026bcb453697370484f218db60
AzorAha1 <wizfaiz@icloud.com>
2024-06-06 20:46:36 +0000

    update

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -14,12 +14,16 @@ describe('calculateNumber', function() {
 		it('check sum of a and b 4...', function() {
                 	assert.strictEqual(calculateNumber(1.5, 3.7), 6)
         	});
-		 it('should work with zero values', function () {
+		it('should work with zero values', function () {
         		const result = calculateNumber(0, 0);
         		assert.equal(result, 0);
 			assert.equal(calculateNumber(0.9, 0.5), 2);
 			assert.equal(calculateNumber(0.4, 0.7), 1);
-		 });
+		});
+		it('should handle negative numbers correctly', function () {
+        		const result = calculateNumber(-3.5, -2.3);
+        		assert.equal(result, -5);
+		});
 
 	});
 });
