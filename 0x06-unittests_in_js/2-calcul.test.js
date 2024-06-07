3eafb34f8002991ff78265bbb7e776a3ff354393
root <root@Bash-Radiant.localdomain>
2024-06-07 20:33:47 +0100

    update

---
+++ 0x06-unittests_in_js/2-calcul.test.js
@@ -0,0 +1,26 @@
+const { expect } = require('chai');
+const calculateNumber = require('./2-calcul_chai');
+
+describe('calculateNumber', function() {
+  describe('sum test', function() {
+    it('should add two positive numbers', function() {
+      expect(calculateNumber('SUM', 1.4, 4.5)).to.equal(4);
+    });
+  });
+
+  describe('subtract test', function() {
+    it('should subtract two numbers', function() {
+      expect(calculateNumber('SUBTRACT', 4.5, 1.4)).to.equal(4);
+    });
+  });
+
+  describe('divide test', function() {
+    it('should divide two positive numbers', function() {
+      expect(calculateNumber('DIVIDE', 1.4, 4.5)).to.equal(0.2);
+    });
+
+    it('should handle division by zero', function() {
+      expect(calculateNumber('DIVIDE', 1.4, 0)).to.equal('Error');
+    });
+  });
+});
