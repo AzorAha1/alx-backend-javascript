a118f3befef32b7a7a2d37147c3a941a718b222a
root <root@Bash-Radiant.localdomain>
2024-06-07 22:51:41 +0100

    2-calcul.test.js

--- 0x06-unittests_in_js/2-calcul.test.js
+++ 0x06-unittests_in_js/2-calcul.test.js
@@ -1,28 +1,28 @@
 const calculateNumber = require('./2-calcul_chai');
+const assert = require('assert');
 const chai = require('chai');
 const expect = chai.expect;
 
 describe('calculateNumber', function() {
   describe('sum test', function() {
     it('should add two positive numbers', function() {
-      expect(calculateNumber('SUM', 1.4, 4.5)).to.equal(6);
+      chai.expect(calculateNumber('SUM', 1.4, 4.5)).to.equal(6);
     });
   });
 
-
   describe('subtract test', function() {
     it('should subtract two numbers', function() {
-      expect(calculateNumber('SUBTRACT', 4.5, 1.4)).to.equal(4);
+      chai.expect(calculateNumber('SUBTRACT', 4.5, 1.4)).to.equal(4);
     });
   });
 
   describe('divide test', function() {
     it('should divide two positive numbers', function() {
-      expect(calculateNumber('DIVIDE', 1.4, 4.5)).to.equal(0.2);
+      chai.expect(calculateNumber('DIVIDE', 1.4, 4.5)).to.equal(0.2);
     });
 
     it('should handle division by zero', function() {
-      expect(calculateNumber('DIVIDE', 1.4, 0)).to.equal('Error');
+      chai.expect(calculateNumber('DIVIDE', 1.4, 0)).to.equal('Error');
     });
   });
 });
