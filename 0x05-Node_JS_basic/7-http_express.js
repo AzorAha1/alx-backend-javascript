5ec3f4f99c8d1d7e60cebbdf41b404a361b2ebaf
root <root@Bash-Radiant.localdomain>
2024-06-08 04:10:55 +0100

    done

---
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const countStudents = require('./3-read_file_async');
+
+const port = 1245;
+const app = express();
+const databasepath = process.argv[2];
+
+app.get('/', (_, response) => {
+  response.send('Hello Holberton School!');
+});
+app.get('/a', async (_, res) => {
+  try {
+    const students = await countStudents(databasepath);
+    res.send(`This is the list of our students\n${students}`);
+  } catch (error) {
+    res.status(500).send('Cannot load the database');
+    console.error(error);
+  }
+});
+app.listen(port, () => {
+  console.log(port);
+});
+module.exports = app;
