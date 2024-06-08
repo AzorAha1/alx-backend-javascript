9b925c2c53ba33f3ee6ce4a3b7ba9f2e53b13d4c
root <root@Bash-Radiant.localdomain>
2024-06-08 01:36:23 +0100

    5-http.js

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -1,32 +1,35 @@
 const http = require('http');
-const fs = require('fs');
+const countStudents = require('./3-read_file_async');
 
-const studentcontentpath = './database.csv';
+const studentcontentpath = process.argv[2];
 const port = 1245;
 const app = http.createServer();
-app.on('request', (request, response) => {
+app.on('request', async (request, response) => {
   const pagebodyinitial = 'Hello Holberton School!';
   const { url } = request;
   if (url === '/') {
     response.statusCode = 200;
     response.setHeader('Content-Type', 'text/plain');
-    response.setHeader('Content-Length', pagebodyinitial.length);
+    response.setHeader('Content-Length', Buffer.byteLength(pagebodyinitial));
     response.end(pagebodyinitial);
   } else if (url === '/students') {
-    fs.readFile(studentcontentpath, (err, content) => {
-      if (err) {
-        response.statusCode = 404;
-        console.error('not found');
-      } else {
-        const title = 'This is the list of our students';
-        response.statusCode = 200;
-        response.setHeader('Content-Type', 'text/csv');
-        response.end(`${title}\n${content}`);
-      }
-    });
+    try {
+      response.statusCode = 200;
+      response.setHeader('Content-Type', 'text/plain');
+      const title = 'This is the list of our students';
+      const studentsdata = await countStudents(studentcontentpath);
+      const responseData = `${title}\n${studentsdata}`;
+      response.setHeader('Content-Length', Buffer.byteLength(responseData));
+      response.end(responseData);
+    } catch (error) {
+      response.statusCode = 500;
+      response.setHeader('Content-Type', 'text/plain');
+      response.end('An error occurred while processing your request');
+      console.log(error);
+    }
   }
 });
 app.listen(port, 'localhost', () => {
-  process.stdout.write(`Server running at port ${port}`);
+  process.stdout.write(`Server running at port ${port}\n`);
 });
 module.exports = app;
