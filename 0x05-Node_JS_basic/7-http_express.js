0b8e2040a45b890f3c4a135eb1312a1734d09cf4
root <root@Bash-Radiant.localdomain>
2024-06-09 16:51:58 +0100

    fix

--- 0x05-Node_JS_basic/7-http_express.js
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -1,10 +1,42 @@
 const express = require('express');
-const countStudents = require('./3-read_file_async');
+const fs = require('fs');
 
 const port = 1245;
 const app = express();
 const databasePath = process.argv[2];
 
+function countStudents(path) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(path, 'utf-8', (err, content) => {
+      if (err) {
+        reject(new Error('Cannot load the database'));
+        return;
+      }
+      const eachline = content.trim().split('\n');
+      const students = eachline.slice(1).filter((line) => line.trim() !== '');
+      let output = `Number of students: ${students.length}\n`;
+      console.log(`Number of students: ${students.length}`);
+      const fields = {};
+      students.forEach((student) => {
+        const studentdata = student.split(',');
+        const firstname = studentdata[0].trim();
+        const field = studentdata[3].trim();
+        if (!fields[field]) {
+          fields[field] = [];
+        }
+        fields[field].push(firstname);
+      });
+      for (const key in fields) {
+        if (Object.prototype.hasOwnProperty.call(fields, key)) {
+          const fieldOutput = `Number of students in ${key}: ${fields[key].length}. List: ${fields[key].join(', ')}`;
+          output += `${fieldOutput}\n`;
+          console.log(fieldOutput);
+        }
+      }
+      resolve(output.trim());
+    });
+  });
+}
 app.get('/', (_, response) => {
   response.set('Content-Type', 'text/plain');
   response.send('Hello Holberton School!');
