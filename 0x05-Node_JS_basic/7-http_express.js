6ce75e21074a7fcb326aa46153ddbe2d60c341e1
root <root@Bash-Radiant.localdomain>
2024-06-09 16:14:42 +0100

    7-http_express.js

--- 0x05-Node_JS_basic/7-http_express.js
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -6,12 +6,12 @@ const app = express();
 const databasepath = process.argv[2];
 
 app.get('/', (_, response) => {
-  response.send(`Hello Holberton School!`);
+  response.send('Hello Holberton School!');
 });
 app.get('/students', async (_, res) => {
   try {
     const students = await countStudents(databasepath);
-    res.send(`This is the list of our students\n${students}`);
+    res.status(200).send(`This is the list of our students\n${students}`);
   } catch (error) {
     res.status(500).send('Cannot load the database');
     console.error(error);
