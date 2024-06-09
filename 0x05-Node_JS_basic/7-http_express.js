49ac618fd1f860f3759ea44f8a09bf523b6f737b
root <root@Bash-Radiant.localdomain>
2024-06-09 17:04:25 +0100

    fixed

--- 0x05-Node_JS_basic/7-http_express.js
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -1,48 +1,47 @@
 const express = require('express');
-const fs = require('fs');
+const { readFile } = require('fs');
 
-const port = 1245;
 const app = express();
+const port = 1245;
 const databasePath = process.argv[2];
 
 function countStudents(path) {
   return new Promise((resolve, reject) => {
-    fs.readFile(path, 'utf-8', (err, content) => {
+    readFile(path, 'utf-8', (err, content) => {
       if (err) {
         reject(new Error('Cannot load the database'));
         return;
       }
-      const eachline = content.trim().split('\n');
-      const students = eachline.slice(1).filter((line) => line.trim() !== '');
+
+      const lines = content.trim().split('\n');
+      const students = lines.slice(1).filter(line => line.trim() !== '');
+
       let output = `Number of students: ${students.length}\n`;
-      console.log(`Number of students: ${students.length}`);
       const fields = {};
+
       students.forEach((student) => {
-        const studentdata = student.split(',');
-        const firstname = studentdata[0].trim();
-        const field = studentdata[3].trim();
+        const [firstname, , , field] = student.split(',');
         if (!fields[field]) {
           fields[field] = [];
         }
         fields[field].push(firstname);
       });
-      for (const key in fields) {
-        if (Object.prototype.hasOwnProperty.call(fields, key)) {
-          const fieldOutput = `Number of students in ${key}: ${fields[key].length}. List: ${fields[key].join(', ')}`;
-          output += `${fieldOutput}\n`;
-          console.log(fieldOutput);
-        }
+
+      for (const [field, names] of Object.entries(fields)) {
+        output += `Number of students in ${field}: ${names.length}. List: ${names.join(', ')}\n`;
       }
+
       resolve(output.trim());
     });
   });
 }
-app.get('/', (_, response) => {
-  response.set('Content-Type', 'text/plain');
-  response.send('Hello Holberton School!');
+
+app.get('/', (req, res) => {
+  res.setHeader('Content-Type', 'text/plain');
+  res.send('Hello Holberton School!');
 });
 
-app.get('/students', async (_, res) => {
+app.get('/students', async (req, res) => {
   try {
     const students = await countStudents(databasePath);
     res.setHeader('Content-Type', 'text/plain');
